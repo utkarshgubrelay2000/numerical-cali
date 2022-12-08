7524c1ef04893934946d666f0fe838728ca48e96
utkarshgubrelay2000 <utkarshgubrelay2000@gmail.com>
2022-12-08 20:02:54 +0530

    unwanted packages removed

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import '../styles/main.scss'
 import '../styles/footer.scss'
 import 'react-toastify/dist/ReactToastify.css';  
 import 'bootstrap/dist/css/bootstrap.css'
-import { RecoilRoot } from 'recoil';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Head from 'next/head';
@@ -71,14 +70,14 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <RecoilRoot>
+    <>
       <Head>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" 
       integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossOrigin="anonymous"></script>
       </Head>
       <Component {...pageProps} />
       <ToastContainer />
-    </RecoilRoot>
+    </>
   )
 }
 
